913026a36a3de48ede1e3695feaac135f887863f
sovaz1997 <sovaz1997@yandex.ru>
2020-03-17 14:32:23 +0300

    feat: test render function

--- js/components/todo-item.js
+++ js/components/todo-item.js
@@ -3,10 +3,17 @@ import Component from "../controller/component.js";
 export default class TodoItem extends Component {
   state = {
     important: false,
-    done: false
+    done: false,
+    text: ''
   }
 
   constructor(props) {
-    super(props);
+    super();
+
+    this.state.text = props;
+  }
+
+  render() {
+    return `<p>${this.state.text}</p>`;
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import Controller from "./controller/controller.js";
 import TodoItem from "./components/todo-item.js";
 
-const app = new Controller(document.querySelector("#app"), new TodoItem);
+const app = new Controller(document.querySelector("#app"), new TodoItem("Тестовый элемент"));
 
